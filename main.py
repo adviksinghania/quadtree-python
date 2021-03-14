2afa273f5345e940cc1b274b59471c4e5d55a4d2
Advik Singhania <adviksinghania@gmail.com>
2021-03-14 21:37:17 +0530

    Modify main script for mouse clicks

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 # main.py
 """Integrating the quadtree algorithm with GUI."""
 
-import time
-import random
 import pygame
 import quadtree
 
@@ -20,39 +18,38 @@ pygame.display.set_caption('Quadtree-Python')
 # Creating a quadtree
 root = quadtree.Rectangle(0, 0, width, height)
 qt = quadtree.Quadtree(root, 4)
-random.seed(time.time())
-for i in range(75):
-    p = quadtree.Point(random.randint(0, width), random.randint(0, height))
-    qt.insert(p)
-
-qt.printsub()
-print()
-
-rects = [eval(str(i)) for i in quadtree.RECTANGLES]
-points = [eval(str(j)) for i in quadtree.POINTS for j in i]
+points = []  # list to store the point clicks
 
 color = (13, 17, 23)  # main window color
 color_rect = (201, 209, 217)  # color for rectangles
 color_point = (54, 213, 83)  # color for points
 
+surface.fill(color)  # filling the main window with color
+
 # Window loop
 running = True
 while running:
-    surface.fill(color)  # filling the main window with color
     # Drawing the root rectangle
     pygame.draw.rect(surface, color_rect, pygame.Rect(0, 0, width, height), 1)
-    # Check if the quit event has happened and exit the loop
-    for event in pygame.event.get():
+    ev_list = pygame.event.get()  # list of events
+    for event in ev_list:
+        # Check if the quit event has happened and exit the loop
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()  # Get coordinates of mouse click
+            if pos not in points:
+                points.append(pos)
+                pygame.draw.circle(surface, color_point, pos, 2)
+                qt.insert(quadtree.Point(*pos))
+                qt.printsub()
 
-    # Drawing the rectangle tree
-    for r in rects:
-        pygame.draw.rect(surface, color_rect, pygame.Rect(*r), 1)
-
-    # Drawing the points
-    for p in points:
-        pygame.draw.circle(surface, color_point, tuple(p.values()), 2)
+            # Drawing the rectangle tree
+            for r in quadtree.RECTANGLES:
+                pygame.draw.rect(surface, color_rect, pygame.Rect(*eval(str(r))), 1)
 
     # Updating the window
     pygame.display.flip()
+
+# Exiting PyGame
+pygame.quit()
